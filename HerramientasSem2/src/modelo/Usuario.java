c3adbc4ae7ff783cea48e966fa2f9d5498e00fbe
zu2220 <u22206853@utp.edu.pe>
2025-04-02 14:37:14 -0500

    Primera versión del proyecto

---
+++ HerramientasSem2/src/modelo/Usuario.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Usuario {
+    private String nombre;
+    private String apellido;
+
+    public Usuario() {
+    }
+    
+    public Usuario(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+}
